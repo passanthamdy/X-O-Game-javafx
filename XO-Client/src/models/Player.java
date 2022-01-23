7fa01a55f7a5ac523d1b5a601a021fbd037cd0f8
passanthamdy <passant.hamdy99@gmail.com>
2022-01-23 23:13:16 +0200

    database classes

---
+++ XO-Client/src/models/Player.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Player {
+	private int player_id;
+	private String username;
+	private String password;
+	private int total_games;
+	private int score;
+	private String status;
+	public Player(int player_id, String username, String password, int total_games, int score, String status) {
+		super();
+		this.player_id = player_id;
+		this.username = username;
+		this.password = password;
+		this.total_games = total_games;
+		this.score = score;
+		this.status = status;
+	}
+	public int getPlayer_id() {
+		return player_id;
+	}
+	public void setPlayer_id(int player_id) {
+		this.player_id = player_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getTotal_games() {
+		return total_games;
+	}
+	public void setTotal_games(int total_games) {
+		this.total_games = total_games;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
--- xo_game.sql
+++ xo_game.sql
@@ -1,111 +1,119 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: game
--- ------------------------------------------------------
--- Server version	8.0.19
+-- Host: localhost:3306
+-- Generation Time: Jan 23, 2022 at 11:00 PM
+-- Server version: 8.0.27-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `game`
+-- Database: `xo_game`
 --
 
-DROP TABLE IF EXISTS `game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `game` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Game`
+--
+
+CREATE TABLE `Game` (
   `game_id` int NOT NULL,
-  `player1` varchar(45) NOT NULL,
-  `player2` varchar(45) NOT NULL,
-  `winner` enum('0','1','2') DEFAULT NULL,
-  PRIMARY KEY (`game_id`),
-  UNIQUE KEY `player1_UNIQUE` (`player1`),
-  UNIQUE KEY `player2_UNIQUE` (`player2`),
-  CONSTRAINT `player1` FOREIGN KEY (`player1`) REFERENCES `players` (`username`),
-  CONSTRAINT `player2` FOREIGN KEY (`player2`) REFERENCES `players` (`username`)
+  `player1` int NOT NULL,
+  `player2` int NOT NULL,
+  `winner` enum('0','1','2') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `game`
+-- Table structure for table `Player`
 --
 
-LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `players`
---
-
-DROP TABLE IF EXISTS `players`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `players` (
+CREATE TABLE `Player` (
   `player_id` int NOT NULL,
   `username` varchar(45) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `total_games` int DEFAULT NULL,
-  `score` int DEFAULT NULL,
-  `status` enum('off','on','ongame') DEFAULT NULL,
-  PRIMARY KEY (`player_id`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
+  `password` varchar(45) NOT NULL,
+  `total_games` int NOT NULL,
+  `score` int NOT NULL,
+  `status` enum('on','off','ongame') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `players`
+-- Dumping data for table `Player`
 --
 
-LOCK TABLES `players` WRITE;
-/*!40000 ALTER TABLE `players` DISABLE KEYS */;
-/*!40000 ALTER TABLE `players` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `Player` (`player_id`, `username`, `password`, `total_games`, `score`, `status`) VALUES
+(1, 'newplayer', '0123456', 3, 30, 'on');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `recordedgame`
+-- Table structure for table `RecordedGames`
 --
 
-DROP TABLE IF EXISTS `recordedgame`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `recordedgame` (
+CREATE TABLE `RecordedGames` (
   `game_id` int NOT NULL,
   `player_id` int NOT NULL,
-  `steps` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`game_id`,`player_id`),
-  KEY `player_id_idx` (`player_id`),
-  CONSTRAINT `game_id` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`),
-  CONSTRAINT `player_id` FOREIGN KEY (`player_id`) REFERENCES `players` (`player_id`)
+  `steps` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `recordedgame`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `recordedgame` WRITE;
-/*!40000 ALTER TABLE `recordedgame` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recordedgame` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `Game`
+--
+ALTER TABLE `Game`
+  ADD PRIMARY KEY (`game_id`),
+  ADD KEY `player1` (`player1`),
+  ADD KEY `Fk` (`player2`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `Player`
+--
+ALTER TABLE `Player`
+  ADD PRIMARY KEY (`player_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `RecordedGames`
+--
+ALTER TABLE `RecordedGames`
+  ADD PRIMARY KEY (`game_id`,`player_id`),
+  ADD KEY `player_id` (`player_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Game`
+--
+ALTER TABLE `Game`
+  ADD CONSTRAINT `Fk` FOREIGN KEY (`player2`) REFERENCES `Player` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Game_ibfk_1` FOREIGN KEY (`player1`) REFERENCES `Player` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `RecordedGames`
+--
+ALTER TABLE `RecordedGames`
+  ADD CONSTRAINT `RecordedGames_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `Game` (`game_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `RecordedGames_ibfk_2` FOREIGN KEY (`player_id`) REFERENCES `Player` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-01-23  1:20:33
